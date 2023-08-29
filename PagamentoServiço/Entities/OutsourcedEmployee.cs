7e2ecbab2806f3b8673518e845fa3c3d32f77a18
wesley1248 <wesleytms.es@gmail.com>
2023-08-28 21:51:41 -0300

    auxilio

---
+++ PagamentoServiço/Entities/OutsourcedEmployee.cs
@@ -0,0 +1,25 @@
+﻿
+namespace PagamentoServiço.Entities {
+    internal class OutsourcedEmployee : Employee {
+        // Herda todos os atributos do Employee
+        public double AdditionalCharger { get; set; }
+
+        public OutsourcedEmployee() { }
+
+        public OutsourcedEmployee(string name, int hours, double valuePerHour, double additionalCharger)
+            /* recebe os dados name, hours.... porem com o ( : base() ) em baixo aproveitas os dados ja
+             * colocados no Employee, restando somente a acreçao do additionalCharger.
+            */
+            : base(name, hours, additionalCharger)
+        {
+            AdditionalCharger = additionalCharger;
+        }
+
+        public override double Payment()
+        // override e´usado para aproveitar os atributos do Payment do Employee.
+        {
+            return base.Payment() + 1.1 * AdditionalCharger;
+            //Feito somente a adiçao da operaçao + 1.1 * AdditionalCharger
+        }
+    }
+}
---
+++ PagamentoServiço/Program.cs
@@ -0,0 +1,64 @@
+﻿using PagamentoServiço.Entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PagamentoServiço {
+    internal class Program {
+        static void Main(string[] args)
+        {
+            List<Employee> List = new List<Employee>();
+            // decalrou  a lista employee 
+
+            Console.Write("Enter the number of employees: ");
+            int numeroFuncionarios = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= numeroFuncionarios; i++)
+            // laço para repetir a quantidade de funcionarios.
+            {
+
+                Console.WriteLine($"Employee #{i} data: ");
+                Console.WriteLine();
+                Console.WriteLine("Outsourced (y/n)?: ");
+                char ch = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                String name = Console.ReadLine();
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine());
+                Console.Write("Value per hour: ");
+                double valuePerHours = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.WriteLine();
+
+                if (ch == 'y')
+                // se a reposta for sim para funcionario tercerizado
+                {
+                    Console.Write("Additional charge: ");
+                    double addicionalCharge = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    // o atributo carga adicional é adionado 
+
+                    List.Add(new OutsourcedEmployee { Name = name, Hours = hours, ValuePerHour = valuePerHours, AdditionalCharger = addicionalCharge });
+                    /* é adicionado a lista(linha 13) um obejto do funcionario tercerizado
+                     * OutsourcedEmployee com a adiçao addicionalCharge carga adicional
+                     */
+                }
+                else
+                {
+                    List.Add(new Employee { Name = name, Hours = hours, ValuePerHour = valuePerHours });
+                    // se for um funcionario normal ele instancia um objeto do funcionario normal Employee
+                        // e adiciona os dados na lista.
+                }
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Payment");
+            foreach (Employee funcionarios in List)
+            {
+                Console.WriteLine("Nome: " + funcionarios.Name + " - $" + funcionarios.Payment().ToString("F2"), CultureInfo.InvariantCulture);
+            }
+        }
+    }
+}
